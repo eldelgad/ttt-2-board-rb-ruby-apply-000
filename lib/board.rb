50ead92f77fc94e805659f679a4fbd9dd88cbf9e
Elias Delgado <evdelgado99@gmail.com>
2020-08-05 20:53:34 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,18 @@
 # Define the variable board below.
 
-row = [" ", " ", " "]
+row = [" ", " ", " ", " ", " ", " ", " ", " "]
 
 row [0]
 
 row [1]
 
 row [2]
+
+row [3]
+
+row [4]
+
+row [ ]
 
 row [1] = "x"
 
