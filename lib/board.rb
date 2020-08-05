621aa27deb212b5f246d7eec4ad9a146435a789e
Elias Delgado <evdelgado99@gmail.com>
2020-08-05 21:24:15 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,41 +1,4 @@
 # Define the variable board below.
 
-board = [" | ", " | ", " | ", " | ", " | ", " | ", " | ", " | ", " | "]
-
-row 1 [" ", " ", " "]
- 
-# Position 1: Left
-row[0]
- 
-# Position 2: Middle
-row[1]
- 
-# Position 3: Right
-row[2]
- 
-row 2 [" ", " ", " "]
-
-# Position 4: Left
-row[3]
- 
-# Position 5: Middle
-row[4]
- 
-# Position 6: Right
-row[5]
-
-row 3 [" ", " ", " "]
-
-# Position 7: Left
-row[6]
- 
-# Position 8: Middle
-row[7]
- 
-# Position 9: Right
-row[8]
-
-board = {row 1, row 2, row 3}
-
-puts board
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
